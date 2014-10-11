7616599442d5951aada1d26bad2467056466a266
Yuriy <sheff_ne@yahoo.com>
2014-10-11 18:59:38 -0500

    Initial Commit

---
+++ UITableViewControllerChallange/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewControllerChallange
+//
+//  Created by Yuriy Shefer on 10/11/14.
+//  Copyright (c) 2014 SHEFFcode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
